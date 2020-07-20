7d322b47e2f0307d6b53ed767515f79544fe2303
tako_eng <oys.sys@gmail.com>
2020-07-20 19:50:01 +0900

    first commit

---
+++ src/servlet/SampleServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SampleServlet")
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 運勢をランダムで決定する
+		String[] luckArray = { "超スッキリ", "スッキリ", "最悪" };
+		int index = (int) (Math.random() * 3);
+		String luck = luckArray[index];
+
+		// 実行日を取得する
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM月DD日");
+		String today = sdf.format(date);
+
+		// HTMLを出力する
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>スッキリ占い</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>" + today + "の運勢は「" + luck + "」です</p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
